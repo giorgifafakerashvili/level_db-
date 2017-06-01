377f858cf2737550a554502eb4a6652926135773
= <giorgi.fafa@gmail.com>
2017-06-02 00:55:09 +0400

    added first.h

---
+++ other/first.h
@@ -0,0 +1,16 @@
+#ifndef _FIRST_H_
+#define _FIRST_H_
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#else
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+#endif
+
+#ifndef __STDC_WANT_LIB_EXT1_
+#define __STDC_WANT_LIB_EXT1_ 1
+#endif
+
+#endif
